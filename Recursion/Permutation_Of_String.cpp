1c9b06923283e0560cf5e464bd265738be043d2d
Rahul Prajapat <85384246+itsPrajapat@users.noreply.github.com>
2021-10-19 16:49:23 +0530

    Recursion Problems Added

---
+++ Recursion/Permutation_Of_String.cpp
@@ -0,0 +1,32 @@
+//  Print all possible permutations of a string
+
+// INPUT = "ABC"
+// OUTPUT = ABC  ACB  BAC  BCA  CAB  CBA
+ 
+#include<iostream>
+using namespace std;
+
+void permutation(string s , string ans)
+{
+    if(s.length()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+
+    for(int i=0; i<s.length(); i++)
+    {
+        char ch = s[i];
+        string ros = s.substr(0,i) + s.substr(i+1);
+
+        permutation(ros , ans+ch);
+    }
+}
+
+int main(){
+
+    permutation("ABC","");
+    
+    return 0;
+}  
+ 
