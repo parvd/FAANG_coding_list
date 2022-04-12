7b2e5f0dfd4c0c2a6fdb7465d0e6da4770dff632
Parv Dave <parvd5@gmail.com>
2022-04-12 17:13:51 +0530

    Added New Questions

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,49 @@
+//In c++
+//by goku_11
+/*
+ 
+*/
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define pb push_back
+#define mp make_pair
+#define MS0(x) memset(x,0,sizeof(x))
+#define MS1(x) memset(x,1,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long int 
+#define llu unsigned long long int
+#define pii pair<int,int>
+#define MOD 1000000007
+#define fi first
+#define sec second
+#define N NULL
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
+{
+return (a.second < b.second);
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("i.txt", "r", stdin);
+freopen("o.txt", "w", stdout);
+#endif
+ll t;
+cin>>t;
+while(t--)
+{
+int n;
+cin>>n;
+int a[n];
+vector<int>vec;
+FOR(i,0,n){
+cin>>a[i];
+}
+
+}
+}
